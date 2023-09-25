3c751565c20dea7934d00f7f5222eaf4d002b0e5
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-09-25 20:36:51 +0300

    Add project files.

---
+++ les7_indexator/Matrix.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les7_indexator
+{
+    internal class Matrix
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        private int[,] _arr;
+
+        public Matrix(int row, int col)
+        {
+            Random rand = new Random();
+
+            Row = row;
+            Col = col;
+
+            _arr = new int[row, col];
+
+            for (int i = 0; i < row; ++i)
+            {
+                for (int j = 0; j < col; ++j)
+                {
+                    _arr[i, j] = rand.Next(0, 9);
+                }
+            }
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if ((i >= 0 && i < _arr.Length) && (j >= 0 && j < _arr.Length))
+                {
+                    return _arr[i, j];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("out of range!");
+                }
+            }
+            set
+            {
+                if ((i >= 0 && i < _arr.Length) && (j >= 0 && j < _arr.Length))
+                {
+                    _arr[i, j] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("out of range!");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Row; ++i)
+            {
+                for (int j = 0; j < Col; ++j)
+                {
+                    sb.Append(this[i, j]);
+                    sb.Append(" ");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            if (m1.Row == m2.Row && m1.Col == m2.Col)
+            {
+                Matrix result = new Matrix(m1.Row, m1.Col);
+
+                for (int i = 0; i < m1.Row; ++i)
+                {
+                    for (int j = 0; j < m1.Col; ++j)
+                    {
+                        result[i, j] = m1[i, j] + m2[i, j];
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("out of range");
+            }
+        }
+    }
+}
---
+++ les7_indexator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les7_indexator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m1 = new Matrix(3, 4);
+            Matrix m2 = new Matrix(3, 4);
+
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+
+            Matrix m3 = m1 + m2;
+            Console.WriteLine(m3);
+        }
+    }
+}
